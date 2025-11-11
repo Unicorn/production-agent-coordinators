08d63468c8aa14604172efeefbdab7bc85c5f408
Matt Bernier <mkbernier@gmail.com>
2025-11-11 13:12:57 -0700

    feat: initialize suite-builder-production package structure

---
+++ packages/agents/suite-builder-production/src/index.ts
@@ -0,0 +1,11 @@
+// Workflows
+export * from './workflows/suite-builder.workflow';
+export * from './workflows/package-build.workflow';
+
+// Activities
+export * from './activities/build.activities';
+export * from './activities/agent.activities';
+export * from './activities/report.activities';
+
+// Types
+export * from './types/index';
