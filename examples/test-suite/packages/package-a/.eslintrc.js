a699571a4ee8dc06d8a33b9fddad92d21ab64027
Matt Bernier <mkbernier@gmail.com>
2025-11-26 11:53:46 -0600

    feat: apply code changes (iteration 9)
    CREATE_OR_OVERWRITE: .eslintrc.js
    
    [Gemini-based turn-based generation - Hybrid Protocol]

--- examples/test-suite/packages/package-a/.eslintrc.js
+++ examples/test-suite/packages/package-a/.eslintrc.js
@@ -1,42 +1,54 @@
+/*
+Copyright (c) 2025 Bernier LLC
+
+This file is licensed to the client under a limited-use license.
+The client may use and modify this code *only within the scope of the project it was delivered for*.
+Redistribution or use in other products or commercial offerings is not permitted without written consent from Bernier LLC.
+*/
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'prettier'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    jest: true
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/await-thenable': 'error',
-    'semi': ['error', 'always'],
-    'indent': ['error', 2, { 'SwitchCase': 1 }],
-    'quotes': ['error', 'single'],
-    'no-var': 'error',
+    // Custom rules and overrides
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Can be too restrictive for utility functions
+    '@typescript-eslint/no-explicit-any': 'error', // Enforce strict typing
+    '@typescript-eslint/no-floating-promises': 'error', // Crucial for async/await
+    '@typescript-eslint/no-inferrable-types': ['warn', {
+      ignoreParameters: true,
+      ignoreProperties: true
+    }],
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off', // Disable base ESLint rule as TS version is used
+    'no-console': ['warn', { allow: ['warn', 'error'] }], // Allow console.warn and console.error
     'prefer-const': 'error',
-    'no-trailing-spaces': 'error',
-    'comma-dangle': ['error', 'always-multiline'],
-    'eol-last': ['error', 'always'],
-    'space-before-function-paren': ['error', {
-      'anonymous': 'never',
-      'named': 'never',
-      'asyncArrow': 'always'
+    '@typescript-eslint/await-thenable': 'error', // Ensure await is used on thenable values
+    '@typescript-eslint/no-misused-promises': ['error', {
+      checksVoidReturn: false // Allow `void` functions to not be awaited if explicitly intended
+    }],
+    '@typescript-eslint/restrict-template-expressions': ['warn', {
+      allowAny: true // Allow string interpolation with any type for simplicity in some cases
     }]
   },
-  ignorePatterns: [
-    'dist/',
-    'node_modules/',
-    '**/*.js'
-  ]
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/']
 };
+```
